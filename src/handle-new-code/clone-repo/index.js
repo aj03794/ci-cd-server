4442a305a83fe6bade049d273a72c0626f815b72
axj1l59 <adam_johnston1@homedepot.com>
2018-05-12 09:27:41 -0400

    Finishing up code for cloning github repo

--- src/handle-new-code/clone-repo/index.js
+++ src/handle-new-code/clone-repo/index.js
@@ -1,6 +1,5 @@
-// import { exec } from 'child_process'
+import { exec } from 'child_process'
 import { resolve as resolvePath } from 'path'
-import clone from 'git-clone'
 
 export const cloneRepo = ({
 	urlToClone
@@ -10,34 +9,22 @@ export const cloneRepo = ({
 	const directoryToCloneRepo = process.env.DIRECTORY_TO_SAVE_REPOS || resolvePath(__dirname, '../../../', 'downloads-from-github')
 	const locationToCloneRepo = resolvePath(directoryToCloneRepo)
 	console.log('locationToCloneRepo', locationToCloneRepo)
-	// exec(`git clone ${urlToClone}`, { cwd: resolvePath(__dirname) }, (err, stdout, stderr) => {
-		// if (err) {
-		// 	return reject({
-		// 		method: 'cloneRepo',
-		// 		data: {
-		// 			successful: false,
-		// 			err
-		// 		}
-		// 	})
-		// }
-		// return resolve({
-		// 	method: 'cloneRepo',
-		// 	data: {
-		// 		successful: true,
-		// 		err: null
-		// 	}
-		// })
-		// if (err) {
-		// 	console.log('err', err)
-		// }
-		// console.log('stdout', stdout)
-		// console.log('stderr', stderr)
-	// })
-	// clone(urlToClone, locationToCloneRepo, err => {
-	// 	if (err) {
-	// 		console.log('err', err)
-	// 		return
-	// 	}
-	// 	console.log('success')
-	// })
+	exec(`git clone ${urlToClone}`, { cwd: locationToCloneRepo }, (err, stdout, stderr) => {
+		if (err) {
+			return reject({
+				method: 'cloneRepo',
+				data: {
+					successful: false,
+					err
+				}
+			})
+		}
+		return resolve({
+			method: 'cloneRepo',
+			data: {
+				successful: true,
+				err: null
+			}
+		})
+	})
 })
