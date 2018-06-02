dad97781ab86aad9bd26b5b9ba3e580aacf8f658
aj03794 <adamjohnston151@yahoo.com>
2018-06-02 19:26:49 -0400

    Fake branch for testing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 
 import { resolve as resolvePath } from 'path'
 
-console.log('hello')
-
 import { redis } from './pubsub/redis'
 import { slack as slackCreator } from './slack'
 import { init } from './server'
