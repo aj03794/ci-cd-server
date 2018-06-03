6f4eace019cccb5347ee84d6eb4c7dee6b9231cf
aj03794 <adamjohnston151@yahoo.com>
2018-06-03 10:54:28 -0400

    Deleting unneeded code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { resolve as resolvePath } from 'path'
 import { redis } from './pubsub/redis'
 import { slack as slackCreator } from './slack'
 import { init } from './server'
-import { exposePort } from './expose-port'
+
 init({
 	redis,
 	slackCreator
